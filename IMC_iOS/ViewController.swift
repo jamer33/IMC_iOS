701a440eb4a14e940ef53a62358386a51b8e2d36
Javier Merino <javier.merino@fluentia.com>
2025-11-05 10:21:41 +0100

    Primer Commit con la versión inicial de la App de IMC

--- IMC_iOS/ViewController.swift
+++ IMC_iOS/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pesoLabel: UILabel!
+    @IBOutlet weak var alturaLabel: UILabel!
+    @IBOutlet weak var imcLabel: UILabel!
+    
+    @IBOutlet weak var pesoControl: UIStepper!
+    @IBOutlet weak var alturaControl: UISlider!
+    
+    var peso : Int = 70
+    var altura : Int = 160
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pesoLabel.text = String(peso)  + " Kg"
+        alturaLabel.text = String(altura) + " Cms"
+        imcLabel.text = "-"
+        pesoControl.value = Double(peso)
+        alturaControl.value = Float(altura)
+        actualizaIMC()
+
     }
 
-
+    @IBAction func pesoSteper(_ sender: UIStepper) {
+        peso = Int(sender.value)
+        pesoLabel.text = String(peso) + " Kg"
+        actualizaIMC()
+    }
+    
+    @IBAction func alturaSlider(_ sender: UISlider) {
+        altura = Int(sender.value)
+        alturaLabel.text = String(altura) + " Cms"
+        actualizaIMC()
+    }
+    
+    func calcularIMC() -> Double {
+        let imc: Double = Double(peso) / pow(Double(altura)/100, 2)
+        return imc
+    }
+    
+    func actualizaIMC() {
+        imcLabel.text = String(format: "%.2f", calcularIMC())
+    }
 }
-
+    
